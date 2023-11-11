9f64484e2a2e10a1e24ce081ba77be4deeac73d3
fatmaelkassaby2003 <125102138+fatmaelkassaby2003@users.noreply.github.com>
2023-11-11 16:59:39 +0200

    Add files via upload

---
+++ task1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= 'My First PHP Page';?></title>
+  </head>
+  <body>
+    <div><?= 'We Love ';?></div>
+    <div><?= 'Elzero Channel ';?></div>
+  </body>
+</html>
+<?php
+      //echo 'Prevent Me From Running Please';
+      #echo 'Prevent Me From Running Please';
+      /*echo 'Prevent Me From Running Please';*/
+
+      /* My Application
+        Version 1.0
+        Created By Elzero*/
+
+// ## First Comment
+# // # Second Comment
+/* /* /* Third Comment */
+////// Fourth Comment
